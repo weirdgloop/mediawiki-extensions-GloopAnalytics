1fd013c77b3c75fc2eaddd19e5a42c8fc47eb000
LucasGamesz <92230930+LucasGamesz@users.noreply.github.com>
2025-03-04 00:17:21 -0300

    Adding Portuguese translations (#2)
    * Create pt.json
    
    * Update GloopAnalytics.i18n.alias.php
    
    * Update pt.json
    
    ---------
    
    Co-authored-by: Jayden Bailey <jaydenkieran@gmail.com>

--- GloopAnalytics.i18n.alias.php
+++ GloopAnalytics.i18n.alias.php
@@ -6,6 +6,10 @@ $specialPageAliases[ 'en' ] = [
 	'Analytics' => [ 'Analytics' ],
 ];
 
+$specialPageAliases[ 'pt' ] = [
+	'Analytics' => [ 'Análises' ],
+];
+
 $specialPageAliases[ 'zh-hans' ] = [
 	'Analytics' => [ '分析' ],
 ];
